d27c8ca7aadedebf31719f943738170fef913d92
DoverDee <doverlee@foxmail.com>
2017-12-05 22:16:04 +0800

    commit goods IPI- two.

--- src/main/java/com/app/controller/GoodsController.java
+++ src/main/java/com/app/controller/GoodsController.java
@@ -19,7 +19,7 @@ public class GoodsController {
   private GoodsService goodsService;
 
   /**
-   * 功能描述：
+   * 功能描述：获取全部商品列表
    */
   @RequestMapping("all")
   @ResponseBody
@@ -29,32 +29,35 @@ public class GoodsController {
   }
 
   /**
-   * 功能描述：
+   * 功能描述：根据商品城市获取商品列表
+   * API_DEMO: 421100000000000 黄冈市
    */
   @RequestMapping("rc")
   @ResponseBody
   public List getGoodsByRc() {
-    List<Goods> goodsList = goodsService.getGoodsByRc("");
+    List<Goods> goodsList = goodsService.getGoodsByRc("421100000000000");
     return goodsList;
   }
 
   /**
-   * 功能描述：
+   * 功能描述：根据商品类型获取商品列表
+   * API_DEMO: SP_YP 优品
    */
   @RequestMapping("tp")
   @ResponseBody
   public List getGoodsByTp() {
-    List<Goods> goodsList = goodsService.getGoodsByTp("");
+    List<Goods> goodsList = goodsService.getGoodsByTp("SP_YP");
     return goodsList;
   }
 
   /**
-   * 功能描述：
+   * 功能描述：根据商品状态获取商品列表
+   * API_DEMO: CS_ZS 在售
    */
   @RequestMapping("stt")
   @ResponseBody
   public List getGoodsByStt() {
-    List<Goods> goodsList = goodsService.getGoodsByStt("");
+    List<Goods> goodsList = goodsService.getGoodsByStt("CS_ZS");
     return goodsList;
   }
 
